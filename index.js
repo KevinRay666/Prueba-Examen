2ea096d10a596dcfeda7acdac33a50accd6aec2a
KevinRay666 <kevinray946@gmail.com>
2023-03-29 22:10:45 -0700

    por arte de magia ya todo quedo

--- index.js
+++ index.js
@@ -1,34 +1,88 @@
 const express = require('express')
+const mysql   = require('mysql')
+const app     = express()
+const port    = 3000
 
-const app = express()
-const port = 3000
 app.use(express.json())
-
-const mysql = require('mysql');
-
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "example",
-    database: "examen",
-    port: "3307"
-});
+    host:"localhost",
+    port:3307,
+    user:"root",
+    password:"example",
+    database:"test"
 
-con.connect(function(err){
-    if(err) throw err;
-    console.log("Connected!")
+})
+con.connect( function (err){
+    if (err){ throw err}
+        console.log("Hola")
 })
 
-app.get('/todo',(req,res) =>{
-    con.query("select * from carros",
-    function(err,result){
-        if(err){
-            throw err;
-        }
-        res.status(200).json(result);
-    })
+
+app.listen(port, () =>{
+    console.log('la app escucha el puerto '+port)
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-});
+app.get("/alumnos",(req,res) => {
+    con.query("select * from alumno",
+    function (err,result){
+        if(err) {throw err}
+        res.status(200).json(result)
+    }
+    )  }
+)
+
+app.post("/alumnos",(req,res) => {
+
+    const alumno = {
+            id:req.body.id,
+            nombre:req.body.nombre,
+            apepat:req.body.apepat,
+            apemat:req.body.apemat
+    }
+
+    let insert = "insert into alumno set ?"
+    con.query(insert,alumno,
+    function (err,result){
+        if(err) {throw err}
+        res.status(200).json({"resultado":"con exito"})
+    }
+    )  }
+)
+
+app.put("/alumnos/:id",(req,res) => {
+
+    const alumno = {
+            id:req.body.id,
+            apemat:req.body.apemat
+    }
+
+    const {apemat} = req.body
+    const {id} = req.params
+
+    let insert = "update alumno set apemat = ? where id = ?"
+    con.query(insert,[apemat,id],
+    function (err,result){
+        if(err) {throw err}
+        res.status(200).json({"resultado":"con exito"})
+    }
+    )  }
+)
+
+
+app.delete("/alumnos/:id",(req,res) => {
+
+    const {id} = req.params
+    let insert = "delete from alumno  where id = ?"
+    con.query(insert,[id],
+    function (err,result){
+        if(err) {throw err}
+        res.status(200).json({"resultado":"borrado con exito"})
+    }
+    )  }
+)
+
+
+app.get('/', function (req, res) {
+    res.send(""+req.body.id);
+    // conslose.log(req)
+  });
