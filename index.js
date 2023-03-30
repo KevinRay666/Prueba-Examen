66b7b55c95cdc69c1bfdb9bb5639df352c49264a
KevinRay666 <kevinray946@gmail.com>
2023-03-29 20:51:44 -0700

    Crear base de datos y leer todos

--- index.js
+++ index.js
@@ -4,6 +4,31 @@ const app = express()
 const port = 3000
 app.use(express.json())
 
+const mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "example",
+    database: "examen",
+    port: "3307"
+});
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log("Connected!")
+})
+
+app.get('/todo',(req,res) =>{
+    con.query("select * from carros",
+    function(err,result){
+        if(err){
+            throw err;
+        }
+        res.status(200).json(result);
+    })
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
